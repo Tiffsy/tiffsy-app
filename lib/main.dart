96b9aaaee9c0125bc0eb521986d13d5d7cc37896
psomani16k <psomani16k@gmail.com>
2023-12-17 21:29:40 +0530

    added some theme data (text fields and drop down menu)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tiffsy_app/firebase_options.dart';
+import 'package:tiffsy_app/screens/AddAddressScreen/screen/add_address_screen.dart';
 import 'package:tiffsy_app/screens/AddressBookScreen/screen/address_book_screen.dart';
 import 'package:tiffsy_app/screens/HomeScreen/screen/home_screen.dart';
 import 'package:tiffsy_app/screens/LoginScreen/screen/login_screen.dart';
@@ -26,9 +27,50 @@ class MyApp extends StatelessWidget {
     var _mediaQuery = MediaQuery.of(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      home: const AddAddressScreen(),
       title: 'Tiffsy',
       theme: ThemeData(
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Color(0xcc121212),
+          selectionHandleColor: Color(0xffffe5a3),
+          selectionColor: Color(0x33ffbe1d),
+        ),
+        dropdownMenuTheme: DropdownMenuThemeData(
+          menuStyle: MenuStyle(
+            backgroundColor: MaterialStateColor.resolveWith(
+              (states) => const Color(0xfffffcef),
+            ),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(4),
+              borderSide: const BorderSide(
+                width: 1,
+                color: Color(0xffffbe1d),
+              ),
+            ),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          labelStyle: const TextStyle(
+            color: Color(0x66121212),
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            letterSpacing: 0.5,
+            height: 24 / 16,
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4),
+            borderSide: const BorderSide(
+              width: 1,
+              color: Color(0xffffbe1d),
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4),
+            borderSide: const BorderSide(width: 1),
+          ),
+        ),
         buttonTheme: const ButtonThemeData(buttonColor: Color(0xffFFBE1D)),
         iconTheme: const IconThemeData(color: Color(0xff323232)),
         elevatedButtonTheme: ElevatedButtonThemeData(

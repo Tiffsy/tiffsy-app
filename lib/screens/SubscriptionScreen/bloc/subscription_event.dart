000a6540acd5d8f6f22c7bc69ff6d2b9e2a8a531
Raj Chordia <rajchordia@Rajs-MacBook-Pro.local>
2024-01-06 09:24:40 +0530

    added subs screen

--- lib/screens/SubscriptionScreen/bloc/subscription_event.dart
+++ lib/screens/SubscriptionScreen/bloc/subscription_event.dart
@@ -6,3 +6,17 @@ sealed class SubscriptionEvent extends Equatable {
   @override
   List<Object> get props => [];
 }
+
+class StartDateChoosenEvent extends SubscriptionEvent {
+  final DateTime startDate;
+  final int noOfdays;
+
+  const StartDateChoosenEvent(
+      {required this.startDate, required this.noOfdays});
+}
+
+class EndDateChoosenEvent extends SubscriptionEvent {
+  final DateTime endDate;
+
+  const EndDateChoosenEvent({required this.endDate});
+}
--- lib/screens/SubscriptionScreen/bloc/subscription_state.dart
+++ lib/screens/SubscriptionScreen/bloc/subscription_state.dart
@@ -2,12 +2,18 @@ part of 'subscription_bloc.dart';
 
 abstract class SubscriptionState extends Equatable {
   const SubscriptionState();
-  
+
   @override
   List<Object> get props => [];
 }
 
-final class SubscriptionInitial extends SubscriptionState {
-  final String subType;
-  SubscriptionInitial({required this.subType});
+final class SubscriptionInitial extends SubscriptionState {}
+
+class NewDatesState extends SubscriptionState {
+  final DateTime startDate;
+  final DateTime endDate;
+
+  const NewDatesState({required this.startDate, required this.endDate});
 }
+
+class EmptyState extends SubscriptionState {}

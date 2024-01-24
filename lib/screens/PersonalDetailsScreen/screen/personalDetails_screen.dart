e664406d6431a65451c4fc627a101c06a52fb515
Parth <psomani16k@gmail.com>
2024-01-24 23:33:54 +0530

    personal details screen fixed

--- lib/screens/PersonalDetailsScreen/screen/personalDetails_screen.dart
+++ lib/screens/PersonalDetailsScreen/screen/personalDetails_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tiffsy_app/Helpers/loading_animation.dart';
 import 'package:tiffsy_app/screens/HomeScreen/screen/home_screen.dart';
 import 'package:tiffsy_app/screens/PersonalDetailsScreen/bloc/personal_details_bloc.dart';
 
@@ -49,8 +50,8 @@ class _PersonalDetailsScreenState extends State<PersonalDetailsScreen> {
           },
           builder: (context, state) {
             if (state is ScreenLoadingScreen) {
-              return const Center(
-                child: CircularProgressIndicator(),
+              return Center(
+                child: LoadingAnimation.circularLoadingAnimation(context),
               );
             } else if (state is PersonalDetailsInitial) {
               if (state.isPhoneAuth) {
@@ -63,9 +64,9 @@ class _PersonalDetailsScreenState extends State<PersonalDetailsScreen> {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         entryBox(name, "Name", AutofillHints.name),
-                        const SizedBox(height: 20),
+                        const SizedBox(height: 24),
                         entryBox(email, "Email ID", AutofillHints.email),
-                        const SizedBox(height: 20),
+                        const SizedBox(height: 40),
                         ElevatedButton(
                           onPressed: () {
                             if (name.text.isNotEmpty &&
@@ -98,15 +99,28 @@ class _PersonalDetailsScreenState extends State<PersonalDetailsScreen> {
                         const EdgeInsets.only(right: 30, left: 30, bottom: 100),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
+                        const SizedBox(height: 80),
+                        const Text(
+                          "Let us know how to address you",
+                          style: TextStyle(
+                            color: Color(0xFF121212),
+                            fontSize: 14,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w500,
+                            height: 20 / 14,
+                            letterSpacing: 0.10,
+                          ),
+                        ),
+                        const SizedBox(height: 40),
                         entryBox(name, "Name", AutofillHints.name),
-                        const SizedBox(height: 20),
+                        const SizedBox(height: 24),
                         entryBox(number, "10-digit Phone Number",
                             AutofillHints.telephoneNumber),
-                        const SizedBox(height: 20),
-                        ElevatedButton(
-                          onPressed: () {
+                        const SizedBox(height: 40),
+                        orderNowButton(
+                          () {
                             if (name.text.isNotEmpty &&
                                 number.text.length == 10) {
                               BlocProvider.of<PersonalDetailsBloc>(context).add(
@@ -121,11 +135,7 @@ class _PersonalDetailsScreenState extends State<PersonalDetailsScreen> {
                                       content: Text("Invalid data entered")));
                             }
                           },
-                          child: const Text(
-                            "Continue",
-                            style: TextStyle(color: Color(0xff121212)),
-                          ),
-                        )
+                        ),
                       ],
                     ),
                   ),
@@ -160,6 +170,41 @@ Widget entryBox(
         fontWeight: FontWeight.w400,
         color: Color(0x66121212),
         height: 24 / 16,
+      ),
+    ),
+  );
+}
+
+Widget orderNowButton(VoidCallback onpress) {
+  Widget buttonText = const Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Spacer(),
+      Text(
+        "Continue",
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 16,
+          color: Color(0xff121212),
+          height: 24 / 16,
+        ),
+      ),
+      Spacer()
+    ],
+  );
+  return InkWell(
+    onTap: () {
+      onpress();
+    },
+    child: Container(
+      constraints: const BoxConstraints(maxHeight: 40),
+      decoration: BoxDecoration(
+        color: const Color(0xffffbe1d),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 10, 24, 10),
+        child: buttonText,
       ),
     ),
   );
